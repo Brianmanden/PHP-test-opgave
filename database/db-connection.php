73cae18909cda739fe181d2622a904ee6ae1c695
Brianmanden <brianmanden@gmail.com>
2020-09-01 18:28:31 +0200

    Refactor DB connection

---
+++ database/db-connection.php
@@ -0,0 +1,22 @@
+<?php
+    // require hidden database credentials
+    require (__DIR__ . '/../secret/db-credentials.php');
+
+    // setting connection variables
+    $servername = $credentials['hostname'];
+    $database = $credentials['base'];
+    $username = $credentials['user'];
+    $password = $credentials['password'];
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo "Connected successfully";
+        return $conn;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+        fwrite("Error: " . $e->getMessage());
+        exit(1);
+    }
+?>
